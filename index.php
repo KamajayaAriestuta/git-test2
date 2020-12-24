de7b97b6040a30b677e4e177b762ac6d1c29d3f6
KamajayaAriestuta <ariestuta11@gmail.com>
2020-12-24 14:40:35 +0800

    menambahkan index.html pada git-test2

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT-Test 2</title>
+</head>
+<body>
+    <h1>Git test 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ipsum architecto labore minima velit repudiandae culpa at quisquam rerum tenetur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+
+    </ul>
+</body>
+</html>
